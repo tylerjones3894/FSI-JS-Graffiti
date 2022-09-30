ccde905bcc6ca2e02f44027c8d29531fc2ba535b
Tyler Jones <tylerjones3894@gmail.com>
2022-09-29 19:48:11 -0700

    updated web content

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+let header = document.querySelector('#page-header')
+    header.style.textAlign = "left"
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
+let dogNames = document.querySelectorAll('.dog-name')
+    for (let i = 0; i < dogNames.length; i++) {
+        dogNames[i].style.textAlign = 'left';
+    }
+let footer = document.querySelector('.footer');
+    footer.style.color = 'blue';
+
+    dogImages.forEach((image) => {
+        image.style.transform = 'rotateY(180deg)';
+    });
+
+
+
 
 // Select some elements...
